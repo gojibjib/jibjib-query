88db539bab097f4136b55473f403bf50d1fd268e
obitech <obitech@users.noreply.github.com>
2018-11-05 16:03:23 +0100

    deleted old response code

--- app/code/util/response.py
+++ app/code/util/response.py
@@ -3,11 +3,6 @@ from flask import jsonify
 def response(status, message, count, data):
     """Returns a JSON encoded API response"""
     
-    # return jsonify(status=status,
-    #                message=message,
-    #                count=count,
-    #                data=data)
-
     return {
         "status": status,
         "message": message,
